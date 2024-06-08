af75e41f4dc8aaa28ed3321960c79ec60057c6dc
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2024-06-08 23:16:21 +0100

    initial commit with app set up and navigation setup

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const AllExpenses = () => {
+  return (
+    <View>
+        <Text>
+            AllExpenses
+        </Text>
+    </View>
+  )
+}
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const RecentExpenses = () => {
+  return (
+    <View>
+        <Text>
+            Recent Expenses
+        </Text>
+    </View>
+  )
+}
